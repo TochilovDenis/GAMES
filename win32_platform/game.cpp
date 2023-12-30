b2725608b4d92b81ab45aa52f43fe78b683bc176
Denis <120037570+TochilovDenis@users.noreply.github.com>
2023-12-30 13:38:51 +0300

    task 1. Раскрасить прямоугольники в разных цветах.

--- win32_platform/game.cpp
+++ win32_platform/game.cpp
@@ -6,13 +6,13 @@ float playerPosX = 0.f;
 float playerPosY = 0.f;
 
 internalVarible void simulateGame(Input* input) {
-	clearScreen(0xff5500);
+	clearScreen(0x8fb398);
 	if (pressed(BUTTON_UP)) playerPosY += 1.f;
 	if (pressed(BUTTON_DOWN)) playerPosY -= 1.f;
 	if (pressed(BUTTON_LEFT)) playerPosX -= 1.f;
 	if (pressed(BUTTON_RIGHT)) playerPosX += 1.f;
 
-	drawRectangle(playerPosX, playerPosY, 1, 1, 0x00ff22);
-	drawRectangle(30, 30, 5, 5, 0xffff22);
-	drawRectangle(-20, 20, 8, 3, 0xffff22);
+	drawRectangle(playerPosX, playerPosY, 1, 1, 0xa265d80);
+	drawRectangle(30, 30, 5, 5, 0xa3881c);
+	drawRectangle(-20, 20, 8, 3, 0xa3881c);
 }
