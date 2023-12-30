d9e15dd3f41698712a106664c148336659548d46
Denis <120037570+TochilovDenis@users.noreply.github.com>
2023-12-30 14:02:06 +0300

    Task 2: все три прямоугольника двигаем при нажатии кнопок-стрелок.

--- win32_platform/game.cpp
+++ win32_platform/game.cpp
@@ -7,12 +7,29 @@ float playerPosY = 0.f;
 
 internalVarible void simulateGame(Input* input) {
 	clearScreen(0x8fb398);
+
+	// Игрок
 	if (pressed(BUTTON_UP)) playerPosY += 1.f;
 	if (pressed(BUTTON_DOWN)) playerPosY -= 1.f;
 	if (pressed(BUTTON_LEFT)) playerPosX -= 1.f;
 	if (pressed(BUTTON_RIGHT)) playerPosX += 1.f;
-
 	drawRectangle(playerPosX, playerPosY, 1, 1, 0xa265d80);
-	drawRectangle(30, 30, 5, 5, 0xa3881c);
-	drawRectangle(-20, 20, 8, 3, 0xa3881c);
+
+	// Прямоугольник 1
+	static float posX1 = 30.f;
+	static float posY1 = 30.f;
+	if (pressed(BUTTON_UP)) posY1 += 1.f;
+	if (pressed(BUTTON_DOWN)) posY1 -= 1.f;
+	if (pressed(BUTTON_LEFT)) posX1 -= 1.f;
+	if (pressed(BUTTON_RIGHT)) posX1 += 1.f;
+	drawRectangle(posX1, posY1, 5, 5, 0xa3881c);
+
+	// Прямоугольник 2
+	static float posX2 = -20.f;
+	static float posY2 = 20.f;
+	if (pressed(BUTTON_UP)) posY2 += 1.f;
+	if (pressed(BUTTON_DOWN)) posY2 -= 1.f;
+	if (pressed(BUTTON_LEFT)) posX2 -= 1.f;
+	if (pressed(BUTTON_RIGHT)) posX2 += 1.f;
+	drawRectangle(posX2, posY2, 8, 3, 0xa3881c);
 }
